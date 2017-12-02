c769bc19f3dcbcd60c9606f0c0069a7b574b28db
Kid Coder <toddacodda@gmail.com>
2017-12-02 07:36:33 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,14 @@
+class Course
+  @@all = []
+  attr_accessor :title, :schedule, :description
+  def initialize
+    @@all << self
+  end
 
-
+  def self.all
+    @@all
+  end
+  def self.reset_all
+    @@all = []
+  end
+end
