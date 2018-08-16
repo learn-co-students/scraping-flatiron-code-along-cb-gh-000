d2bee57185b88472b65ec17857914df38fe55f72
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-16 17:39:31 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,6 +16,25 @@ class Scraper
     end
   end
   
+  def get_page
+     Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
+  end
+  
+  def get_courses
+    # coursesArray = []
+    Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses')).css('article')
+    # coursesArray
+  end
+  
+  def make_courses
+    get_courses.each{|course| 
+      cours = Course.new
+      cours.title = course.css('h2').text 
+      cours.description = course.css('p').text 
+      cours.schedule = course.css('.date').text
+    }
+  end
+  
 end
 
 
